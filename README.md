c381365a1e9b9b7909582b1f6fe3bb0d3b3ba352
Hanra <37299738+HenraL@users.noreply.github.com>
2022-08-15 23:16:42 +0200

    Created the python module ask_question

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ask_question
+
 This is a python package I created in order to simplify the boiling process when asking the user a question via TTY.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ask-question',  
+    version='1.0.0',
+    scripts=['src/ask_question.py'] ,
+    author="Henry Letellier",
+    author_email="henrysoftwarehouse@protonmail.com",
+    description="A module simplifie the boiling process when asking the user a question via TTY.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hanra-s-work/ask_question",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: THE UNLICENSED",
+        "Operating System :: OS Independent",
+    ],
+)
