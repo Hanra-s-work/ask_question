3ea1eea52fd3f49feb59848cbbfb3f9449111056
Hanra <37299738+HenraL@users.noreply.github.com>
2023-09-08 09:40:32 +0200

    [UPDATE] called token + [UPDATE] the .gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8", newline="\n") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='ask-question',
+    name='ask_question',
     version='1.2.0',
     packages=setuptools.find_packages(),
     install_requires=[
