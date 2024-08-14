23aa1098dd73df4497f49609dc9e0f6345adbc50
Hanra <37299738+HenraL@users.noreply.github.com>
2024-08-14 17:47:47 +0300

    [UPDATE] asciimativ_overlay_ov version to 1.0.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     version='1.2.0',
     packages=setuptools.find_packages(),
     install_requires=[
-        "asciimatics-overlay-ov==1.0.5"
+        "asciimatics-overlay-ov==1.0.6"
     ],
     author="Henry Letellier",
     author_email="henrysoftwarehouse@protonmail.com",
