8f400f947af56796979be5c61a0358af68a00c4c
Hanra <37299738+HenraL@users.noreply.github.com>
2024-08-14 17:25:25 +0300

    [UPDATE] dependencies in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setuptools.setup(
     version='1.2.0',
     packages=setuptools.find_packages(),
     install_requires=[
-        "asciimatics==1.14.0",
-        "python-magic==0.4.27",
         "asciimatics-overlay-ov==1.0.3"
     ],
     author="Henry Letellier",
